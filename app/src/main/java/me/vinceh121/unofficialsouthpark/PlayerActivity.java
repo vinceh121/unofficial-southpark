d45569885d5b34626c2b1f4a39996fe32a8846c5
vinceh121 <contact@vinceh121.me>
2022-02-06 00:00:27 +0100

    Force WebVTT for subs

--- app/src/main/java/me/vinceh121/unofficialsouthpark/PlayerActivity.java
+++ app/src/main/java/me/vinceh121/unofficialsouthpark/PlayerActivity.java
@@ -23,82 +23,84 @@ import java.util.Vector;
 import me.vinceh121.unofficialsouthpark.entities.MediaInfo;
 
 public class PlayerActivity extends AppCompatActivity {
-	private SimpleExoPlayer player;
+    private SimpleExoPlayer player;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_player);
-		this.getSupportActionBar().hide();
-		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+        this.getSupportActionBar().hide();
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-		final StyledPlayerView playerView = findViewById(R.id.player_view);
+        final StyledPlayerView playerView = findViewById(R.id.player_view);
 
-		playerView.setControllerOnFullScreenModeChangedListener(new StyledPlayerControlView.OnFullScreenModeChangedListener() {
-			@Override
-			public void onFullScreenModeChanged(final boolean isFullScreen) {
-				if (isFullScreen) {
-					playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE);
-				} else {
-					playerView.setSystemUiVisibility(0);
-				}
-			}
-		});
+        playerView.setControllerOnFullScreenModeChangedListener(new StyledPlayerControlView.OnFullScreenModeChangedListener() {
+            @Override
+            public void onFullScreenModeChanged(final boolean isFullScreen) {
+                if (isFullScreen) {
+                    playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE);
+                } else {
+                    playerView.setSystemUiVisibility(0);
+                }
+            }
+        });
 
-		player = new SimpleExoPlayer.Builder(this).build();
-		playerView.setPlayer(player);
+        player = new SimpleExoPlayer.Builder(this).build();
+        playerView.setPlayer(player);
 
-		final ArrayList<MediaInfo> infos = (ArrayList<MediaInfo>) getIntent().getSerializableExtra("mediaInfo");
+        final ArrayList<MediaInfo> infos = (ArrayList<MediaInfo>) getIntent().getSerializableExtra("mediaInfo");
 
-		for (final MediaInfo mediaInfo : infos) {
-			final Uri uri = Uri.parse(mediaInfo.getRendition().get(0).getSrc());
-			final MediaItem.Builder build = new MediaItem.Builder()
-					.setMimeType(MimeTypes.APPLICATION_M3U8)
-					.setUri(uri);
+        for (final MediaInfo mediaInfo : infos) {
+            final Uri uri = Uri.parse(mediaInfo.getRendition().get(0).getSrc());
+            final MediaItem.Builder build = new MediaItem.Builder()
+                    .setMimeType(MimeTypes.APPLICATION_M3U8)
+                    .setUri(uri);
 
-			if (PreferenceManager.getDefaultSharedPreferences(this).getBoolean("subtitles", true)) {
-				final List<MediaItem.SubtitleConfiguration> subs = new Vector<>();
-				for (final MediaInfo.Transcript trans : mediaInfo.getTranscript()) {
-					for (final MediaInfo.Typographic typo : trans.getTypographic()) {
-						subs.add(new MediaItem.SubtitleConfiguration.Builder(Uri.parse(typo.getSrc()))
-								.setSelectionFlags(C.SELECTION_FLAG_FORCED)
-								.setMimeType(getSubMimetype(typo.getFormat()))
-								.build());
-					}
-				}
-				build.setSubtitleConfigurations(subs);
-			}
+            if (PreferenceManager.getDefaultSharedPreferences(this).getBoolean("subtitles", true)) {
+                final List<MediaItem.SubtitleConfiguration> subs = new Vector<>();
+                for (final MediaInfo.Transcript trans : mediaInfo.getTranscript()) {
+                    for (final MediaInfo.Typographic typo : trans.getTypographic()) {
+                        if (typo.getFormat().equals("vtt")) {
+                            subs.add(new MediaItem.SubtitleConfiguration.Builder(Uri.parse(typo.getSrc()))
+                                    .setSelectionFlags(C.SELECTION_FLAG_FORCED)
+                                    .setMimeType(getSubMimetype(typo.getFormat()))
+                                    .build());
+                        }
+                    }
+                }
+                build.setSubtitleConfigurations(subs);
+            }
 
-			final MediaItem item = build.build();
+            final MediaItem item = build.build();
 
-			player.addMediaItem(item);
-		}
+            player.addMediaItem(item);
+        }
 
-		player.prepare();
-		player.play();
-	}
+        player.prepare();
+        player.play();
+    }
 
-	@Override
-	protected void onPause() {
-		super.onPause();
-		player.pause();
-	}
+    @Override
+    protected void onPause() {
+        super.onPause();
+        player.pause();
+    }
 
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		player.release();
-	}
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        player.release();
+    }
 
-	public static String getSubMimetype(final String apiType) {
-		switch (apiType) {
-			case "ttml":
-				return MimeTypes.APPLICATION_TTML;
-			case "vtt":
-				return MimeTypes.TEXT_VTT;
-			case "cea-608":
-				return MimeTypes.APPLICATION_CEA608;
-		}
-		return null;
-	}
+    public static String getSubMimetype(final String apiType) {
+        switch (apiType) {
+            case "ttml":
+                return MimeTypes.APPLICATION_TTML;
+            case "vtt":
+                return MimeTypes.TEXT_VTT;
+            case "cea-608":
+                return MimeTypes.APPLICATION_CEA608;
+        }
+        return null;
+    }
 }
